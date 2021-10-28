fb3070b38c50509e43b48bfa29df880373e3a30e
Areeya Suwannathot <88705670+areeyaswnth@users.noreply.github.com>
2021-10-29 02:36:45 +0700

    scoreupdate

--- Score.cpp
+++ Score.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 Score::Score()
 {
-	i = 0;
+	i = 0;	
+	scorefile.open("./score.txt");
+	while (scorefile>>temp>>score[i]&&i<5)
+	{
+		cout << temp<<" " << score[i]<< "\n";
+		name[i] = temp;
+		this->userScore.push_back(make_pair(this->score[5],this->name[5]));		
+		i++;	
+	}	
+	scorefile.close();
+	for (int i = 0; i < 5; i++)
+	{
+		cout << score[i] <<" "<< name[i] << "\n";
+	}
 }
 
 Score::~Score()
@@ -13,11 +26,6 @@ Score::~Score()
 void Score::ReadFile()
 {
 	
-	scorefile.open("./score.txt");
-	while (scorefile>>temp>>score[i]&&i<5)
-	{
-		cout << temp<<" " << score[i]<< "\n";
-		i++;
-	}
-	scorefile.close();
+
+
 }
