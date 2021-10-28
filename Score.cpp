dc69599891ad3e00d9572b48662e12b076b738ad
Areeya Suwannathot <88705670+areeyaswnth@users.noreply.github.com>
2021-10-29 03:01:13 +0700

    pair

--- Score.cpp
+++ Score.cpp
@@ -11,12 +11,9 @@ Score::Score()
 		name[i] = temp;
 		this->userScore.push_back(make_pair(this->score[5],this->name[5]));		
 		i++;	
-	}	
+	}		
 	scorefile.close();
-	for (int i = 0; i < 5; i++)
-	{
-		cout << score[i] <<" "<< name[i] << "\n";
-	}
+
 }
 
 Score::~Score()
@@ -25,7 +22,13 @@ Score::~Score()
 
 void Score::ReadFile()
 {
-	
+		scorefile.open("./score.txt");
+	//sort(score[5]);
+	for (int i = 0; i < 5; i++)
+	{
+		scorefile <<score[i]  <<" "<<name[i]<<"\n";
+	}	
+	scorefile.close();
 
 
 }
--- Score.h
+++ Score.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include<algorithm>
 class Score
 {
 public:
